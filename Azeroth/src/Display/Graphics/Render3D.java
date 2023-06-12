1edd669c77e76134ea2731ea23923d871387f0d2
AzЯaeL <96765154+LucidSyntax@users.noreply.github.com>
2023-06-12 15:58:14 -0400

    Add files via upload

--- Azeroth/src/Display/Graphics/Render3D.java
+++ Azeroth/src/Display/Graphics/Render3D.java
@@ -1,5 +1,23 @@
 package Display.Graphics;
 
-public class Render3D {
-    
+import java.lang.reflect.Constructor;
+
+public class Render3D extends Render{
+    public Render3D(int width, int height){
+        super(width, height);
+    }
+
+    public void floor() {
+        for (int y = 0; y < height; y++) {
+            double yDepth = y - height / 2;
+            double z = 100.0 / yDepth;
+
+            for (int x = 0; x < width; x++){
+                double depth = x - width / 2;
+                depth *= z;
+                int xx = (int) (depth) & 5; // xx converts depth to integer
+                pixels[x + y * width] = xx * 128;
+            }
+        }
+    }
 }
--- Azeroth/src/Display/Graphics/Screen.java
+++ Azeroth/src/Display/Graphics/Screen.java
@@ -7,10 +7,12 @@ import Display.Game;
 public class Screen extends Render { // this class is for generating pixels and adjusting screen 
 
     private Render test; // this will be used to test things out
-
+    private Render3D render;
     public Screen(int width, int height) {
             super(width, height); // means it is inheirting qualities from the render class, like width and height
             Random random = new Random(); // randomizes the pixels
+            render = new Render3D(width, height);
+
             test = new Render(256, 256); // creating limits to width and height  
             for (int i = 0; i < 256 * 256; i++) {
                 test.pixels[i] = random.nextInt() * (random.nextInt(5) / 4); // pixels value will be randomized, because of this what ever random number pixels is will be converted to an rgb value
@@ -26,8 +28,9 @@ public class Screen extends Render { // this class is for generating pixels and 
 
                 int anim = (int) (Math.sin((game.time + i * 2) % 1000.0 / 100) * 100);
                 int anim2 = (int) (Math.cos((game.time + i * 2) % 1000.0 / 100) * 100);
-                draw(test, (width - 256) / 2 + anim, (height - 256) / 2 + anim2); // calls the draw method in the render class, then sets both y and x offsets to 0 || (width - 256) / 2 puts screen in the middle
             }
+            render.floor();
+            draw(render, 0, 0); 
         } 
 }
  
